4bfed7cac403e46797d14c3bd16e6e22355b1bc4
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-29 00:00:51 +0530

    .

--- DSA Question/74_question_leetcode.cpp
+++ DSA Question/74_question_leetcode.cpp
@@ -37,20 +37,20 @@
 class Solution {
 public:
 
-    bool binarySearch(vector<int>& nums, int target) {
-        // aproach -2
-    int n = nums.size(); //size of the array
-    int low = 0, high = n - 1;
+//     bool binarySearch(vector<int>& nums, int target) {
+//         // aproach -2
+//     int n = nums.size(); //size of the array
+//     int low = 0, high = n - 1;
 
-    // Perform the steps:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (nums[mid] == target) return true;
-        else if (target > nums[mid]) low = mid + 1;
-        else high = mid - 1;
-    }
-    return false;
-}
+//     // Perform the steps:
+//     while (low <= high) {
+//         int mid = (low + high) / 2;
+//         if (nums[mid] == target) return true;
+//         else if (target > nums[mid]) low = mid + 1;
+//         else high = mid - 1;
+//     }
+//     return false;
+// }
 
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
